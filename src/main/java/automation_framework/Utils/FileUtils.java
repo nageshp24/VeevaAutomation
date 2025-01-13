57dd0375c487a2460929d16b0509ede2f1ca4a2d
nageshp24 <100015356+nageshp24@users.noreply.github.com>
2025-01-13 10:27:39 +0530

    Update FileUtils.java

--- src/main/java/automation_framework/Utils/FileUtils.java
+++ src/main/java/automation_framework/Utils/FileUtils.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileUtils {
+    // Write data to file
     public static void writeToFile( String content) {
         try (FileWriter writer = new FileWriter("src/Output/ProductDetailsFile.txt", true)) {
             writer.write(content);
@@ -11,7 +12,7 @@ public class FileUtils {
             e.printStackTrace();
         }
     }
-
+ // Write data to csv file
     public static void writeToCSVFile( String content) {
         try (FileWriter writer = new FileWriter("src/Output/DP2_LinksFile.csv", true)) { // Use try-with-resources for auto-closing
             writer.write(content);
@@ -19,7 +20,7 @@ public class FileUtils {
             e.printStackTrace();
         }
     }
-
+// Write copy file
     public static boolean copyFile(File sourceFilePath, File destinationFilePath) {
 
         // Copy the file using Apache Commons IO
